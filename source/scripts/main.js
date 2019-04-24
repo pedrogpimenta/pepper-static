fbf0cf854a51b4f66ccb51401b44512f5d353b39
Pedro Pimenta <pedro@pimenta.co>
2019-04-24 16:46:02 +0200

    Update and remove unneded content

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,17 +1,2 @@
-// Write your JS herea
-// const tether = require('tether');
-// import Tether from 'tether';
-import Drop from 'tether-drop';
+// Write your JS here
 
-const handleSubmenus = function() {
-  console.log('ye');
-  const newDrop = new Drop({
-    target: document.querySelector('.has-submenu'),
-    content: 'hola coño',
-    position: 'bottom left',
-    openOn: 'hover'
-  });
-  newDrop.caca = true;
-};
-
-handleSubmenus();
